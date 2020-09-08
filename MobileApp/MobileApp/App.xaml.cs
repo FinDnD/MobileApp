20ed13c7bf8ba03d71601a3d3e66daf9a6d8f925
Nicco Ryan <niccoryan0@gmail.com>
2020-09-07 18:45:07 -0700

    PLaying with Startup

--- MobileApp/MobileApp/App.xaml.cs
+++ MobileApp/MobileApp/App.xaml.cs
@@ -3,11 +3,15 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using MobileApp.Services;
 using MobileApp.Views;
+using MobileApp.Models.DTOs;
 
 namespace MobileApp
 {
     public partial class App : Application
     {
+        public static PlayerDTO CurrentPlayer;
+        public static DungeonMasterDTO CurrentDM;
+        public static string UserToken;
 
         public App()
         {

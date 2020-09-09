90054a4da29baf17363c66e96dd8417fcae21399
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-09-09 16:49:08 -0700

    Updated Login page buttons. Added a DM content page in profile page

--- MobileApp/MobileApp/Views/ProfilePage.xaml.cs
+++ MobileApp/MobileApp/Views/ProfilePage.xaml.cs
@@ -15,6 +15,265 @@ namespace MobileApp.Views
         public ProfilePage()
         {
             InitializeComponent();
+            
+            //if (App.CurrentPlayer != null)
+            //{
+            //    Children.Add(CreatePlayerContent);
+            //}
+            //else
+            //{
+            //    Children.Add(CreateDMContent);
+            //}
+
+        }
+
+        public ContentPage CreateDMContent()
+        {
+            ContentPage page = new ContentPage();
+
+            Grid grid = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition {Height = 10},
+                    new RowDefinition {Height = 80},
+                    new RowDefinition {Height = 10}
+                }
+            };
+
+            StackLayout stackLayout1 = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Margin = new Thickness(10, 10, 0, 10),
+                Padding = new Thickness(0, 20, 0, 0)
+            };
+
+            Image imageLogo = new Image
+            {
+                Source = "dice.icon50.png",
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            Label labelHeadline = new Label
+            {
+                Text = "My Character",
+                Padding = new Thickness(0, 10, 0, 0),
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                TextColor = Color.White
+            };
+
+            StackLayout stackLayout2 = new StackLayout
+            {
+                Margin = new Thickness(10, 10, 0, 10),
+                Padding = new Thickness(0, 20, 0, 0)
+            };
+
+            Label labelCampaign= new Label
+            {
+                Text = "Campaign Name",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Margin = new Thickness(10, 10, 0, 10)
+            };
+
+            Label labelExperience = new Label
+            {
+                Text = "Experience Level",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Margin = new Thickness(10, 10, 0, 10)
+            };
+
+            Label labelCampaignDescription = new Label
+            {
+                Text = "Campaign Description",
+                TextColor = Color.Gray,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Start,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+
+            Editor editorCampaignDescription = new Editor
+            {
+                TextColor = Color.Gray,
+                HeightRequest = 80,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Placeholder  = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Vitae elementum curabitur vitae nunc sed velit dignissim sodales ut. Fusce id velit ut tortor. Cras adipiscing enim eu turpis. Non enim praesent elementum facilisis leo vel fringilla. Suspendisse potenti nullam ac tortor vitae purus. Tristique et egestas quis ipsum suspendisse ultrices gravida."
+            };
+
+            Label labelPersonalBio = new Label
+            {
+                Text = "Personal Bio",
+                TextColor = Color.Gray,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Start,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+
+            Editor editorPersonalBio = new Editor
+            {
+                TextColor = Color.Gray,
+                HeightRequest = 80,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Placeholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Vitae elementum curabitur vitae nunc sed velit dignissim sodales ut. Fusce id velit ut tortor. Cras adipiscing enim eu turpis. Non enim praesent elementum facilisis leo vel fringilla. Suspendisse potenti nullam ac tortor vitae purus. Tristique et egestas quis ipsum suspendisse ultrices gravida."
+            };
+
+            Button button = new Button
+            {
+                Text = "DELETE CHARACTER",
+                BackgroundColor = Color.FromHex("#9D0A0E"),
+                TextColor = Color.White,
+                HeightRequest = 50,
+                VerticalOptions = LayoutOptions.Start,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+
+            // STACK1
+            stackLayout1.Children.Add(imageLogo);
+            stackLayout1.Children.Add(labelHeadline);
+
+            // STACK2
+            stackLayout2.Children.Add(labelCampaign);
+            stackLayout2.Children.Add(labelExperience);
+            stackLayout2.Children.Add(labelCampaignDescription);
+            stackLayout2.Children.Add(editorCampaignDescription);
+            stackLayout2.Children.Add(labelPersonalBio);
+            stackLayout2.Children.Add(editorPersonalBio);
+            stackLayout2.Children.Add(button);
+
+            Grid.SetRow(stackLayout2, 1);
+            //Grid.SetRow((BindableObject)view, row);
+
+            //GRID
+            grid.Children.Add(stackLayout1);
+            grid.Children.Add(stackLayout2);
+
+            return page;
+        }
+
+
+        public ContentPage CreatePlayerContent()
+        {
+            ContentPage page = new ContentPage();
+
+            Grid grid = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition {Height = 10},
+                    new RowDefinition {Height = 80},
+                    new RowDefinition {Height = 10}
+                }
+            };
+
+            StackLayout stackLayout1 = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Margin = new Thickness(10, 10, 0, 10),
+                Padding = new Thickness(0, 20, 0, 0)
+            };
+
+            Image imageLogo = new Image
+            {
+                Source = "dice.icon50.png",
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            Label labelHeadline = new Label
+            {
+                Text = "My Character",
+                Padding = new Thickness(0, 10, 0, 0),
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                TextColor = Color.White
+            };
+
+            StackLayout stackLayout2 = new StackLayout
+            {
+                Margin = new Thickness(10, 10, 0, 10),
+                Padding = new Thickness(0, 20, 0, 0)
+            };
+
+            Label labelCampaign = new Label
+            {
+                Text = "Campaign Name",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Margin = new Thickness(10, 10, 0, 10)
+            };
+
+            Label labelExperience = new Label
+            {
+                Text = "Experience Level",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Margin = new Thickness(10, 10, 0, 10)
+            };
+
+            Label labelCampaignDescription = new Label
+            {
+                Text = "Campaign Description",
+                TextColor = Color.Gray,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Start,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+
+            Editor editorCampaignDescription = new Editor
+            {
+                TextColor = Color.Gray,
+                HeightRequest = 80,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Placeholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Vitae elementum curabitur vitae nunc sed velit dignissim sodales ut. Fusce id velit ut tortor. Cras adipiscing enim eu turpis. Non enim praesent elementum facilisis leo vel fringilla. Suspendisse potenti nullam ac tortor vitae purus. Tristique et egestas quis ipsum suspendisse ultrices gravida."
+            };
+
+            Label labelPersonalBio = new Label
+            {
+                Text = "Personal Bio",
+                TextColor = Color.Gray,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Start,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+
+            Editor editorPersonalBio = new Editor
+            {
+                TextColor = Color.Gray,
+                HeightRequest = 80,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                Placeholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Vitae elementum curabitur vitae nunc sed velit dignissim sodales ut. Fusce id velit ut tortor. Cras adipiscing enim eu turpis. Non enim praesent elementum facilisis leo vel fringilla. Suspendisse potenti nullam ac tortor vitae purus. Tristique et egestas quis ipsum suspendisse ultrices gravida."
+            };
+
+            Button button = new Button
+            {
+                Text = "DELETE CHARACTER",
+                BackgroundColor = Color.FromHex("#9D0A0E"),
+                TextColor = Color.White,
+                HeightRequest = 50,
+                VerticalOptions = LayoutOptions.Start,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+
+            // STACK1
+            stackLayout1.Children.Add(imageLogo);
+            stackLayout1.Children.Add(labelHeadline);
+
+            // STACK2
+            stackLayout2.Children.Add(labelCampaign);
+            stackLayout2.Children.Add(labelExperience);
+            stackLayout2.Children.Add(labelCampaignDescription);
+            stackLayout2.Children.Add(editorCampaignDescription);
+            stackLayout2.Children.Add(labelPersonalBio);
+            stackLayout2.Children.Add(editorPersonalBio);
+            stackLayout2.Children.Add(button);
+
+            Grid.SetRow(stackLayout2, 1);
+            //Grid.SetRow((BindableObject)view, row);
+
+            //GRID
+            grid.Children.Add(stackLayout1);
+            grid.Children.Add(stackLayout2);
+
+            return page;
         }
 
         void OnToggled(object sender, ToggledEventArgs e)
@@ -25,5 +284,7 @@ namespace MobileApp.Views
 
             }*/
         }
+
+
     }
 }
